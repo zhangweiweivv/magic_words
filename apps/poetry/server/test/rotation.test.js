03e76170c55694b4856b44ce7aa442872c9704da
vvhome <vvhome@vvhomedeMac-mini.local>
2026-04-11 16:15:22 +0800

    fix(poetry): remove dead _resetCursorForTest import (I3)

--- apps/poetry/server/test/rotation.test.js
+++ apps/poetry/server/test/rotation.test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 
-const { getNextArticle, _resetCursorForTest } = require('../services/rotation');
+const { getNextArticle } = require('../services/rotation');
 
 describe('rotation', () => {
   let tmpDir;
